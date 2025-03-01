bf8c760c4d4c6319765f2cc3829ab436259d9821
naylasyitha <naylasyitha30@gmail.com>
2025-03-01 10:07:14 +0700

    Validasi

---
+++ internal/app/user/interface/rest/user.go
@@ -0,0 +1,72 @@
+package rest
+
+import (
+	"innovaspace/internal/app/user/usecase"
+	"innovaspace/internal/domain/dto"
+	"innovaspace/internal/validation"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserHandler struct {
+	usecase   usecase.UserUsecaseItf
+	validator validation.InputValidation
+}
+
+func NewUserHandler(routerGroup fiber.Router, userUsecase usecase.UserUsecaseItf, Validator validation.InputValidation) {
+	userHandler := UserHandler{
+		usecase:   userUsecase,
+		validator: Validator,
+	}
+
+	routerGroup = routerGroup.Group("/users")
+	routerGroup.Post("/register", userHandler.Register)
+	routerGroup.Post("/login", userHandler.Login)
+}
+
+func (h *UserHandler) Register(ctx *fiber.Ctx) error {
+	var register dto.Register
+
+	if err := ctx.BodyParser(&register); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request body",
+		})
+	}
+
+	if err := h.validator.Validate(register); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	if err := h.usecase.Register(register); err != nil {
+		return err
+	}
+
+	return ctx.Status(http.StatusCreated).JSON(fiber.Map{
+		"message": "success register user",
+	})
+
+}
+
+func (h *UserHandler) Login(ctx *fiber.Ctx) error {
+	var login dto.Login
+
+	if err := ctx.BodyParser(&login); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request body",
+		})
+	}
+
+	token, err := h.usecase.Login(login)
+	if err != nil {
+		return err
+
+	}
+
+	return ctx.Status(http.StatusOK).JSON(fiber.Map{
+		"message": "success login user",
+		"token":   token,
+	})
+}
---
+++ internal/domain/entity/user.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	UserId       uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
+	Email        string    `json:"email" gorm:"type:varchar(255);unique"`
+	Username     string    `json:"username" gorm:"type:varchar(255);unique"`
+	Password     string    `json:"password" gorm:"type:varchar(255)"`
+	Nama         string    `json:"nama" gorm:"type:varchar(255)"`
+	Institusi    string    `json:"institusi" gorm:"type:varchar(255)"`
+	BidangBisnis string    `json:"bidang_bisnis" gorm:"type:varchar(255)"`
+	IsPremium    bool      `json:"is_premium" gorm:"type:boolean;default:false"`
+	PremiumStart time.Time `json:"premium_start" gorm:"type:timestamp"`
+	PremiumEnd   time.Time `json:"premium_end"`
+	CreatedDate  time.Time `json:"created_date" gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
+	ModifiedDate time.Time `json:"modified_date" gorm:"type:datetime;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+}
